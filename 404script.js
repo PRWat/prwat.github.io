581b84a6deea077a4cc10795bfb2d54885c62929
Peter Watanabe <peterwatanabe@gmail.com>
2015-10-14 18:45:16 -0700

    404 testing

---
+++ 404script.js
@@ -0,0 +1,10 @@
+/*
+*   JavaScript sourcecode for prwat.github.io profile site
+*   github will automatically load 404.html in the event of a 404.
+*   404.html contains only this script, which examines the URL and 
+*   Author: Peter Watanabe
+*/
+
+$(document).ready(function () {
+    alert("DOOP");
+    });
