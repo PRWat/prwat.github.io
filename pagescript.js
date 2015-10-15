cce370b8a07205c687cec39d76d92f1870034bac
Peter Watanabe <peterwatanabe@gmail.com>
2015-10-14 21:47:50 -0700

    404 Testing

--- pagescript.js
+++ pagescript.js
@@ -5,6 +5,18 @@
 */
 
 $(document).ready(function () {
+    var urlAnchor = location.hash;
+    urlAnchor = urlAnchor.substr(1);
+    $("#navbar ul li").removeClass("active");
+    if (urlAnchor == "") {
+        $("#navbar ul li#home").addClass("active");
+    } else {
+        var test = "#navbar ul li#" + urlAnchor;
+        $(test).addClass("active");
+    }
+
+    $("#maincontent").load(urlAnchor + ".html");
+    
     $("#navbar ul li").click(function (eventObject) {
         var clickedElement = $(this).attr("id");
         $("#navbar ul li").removeClass("active");
